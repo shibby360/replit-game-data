15f1a8845d35aa0c7a3dd27625c86f05cef7bd68
shibby360 <shivank.chhaya@gmail.com>
2024-01-31 04:38:55 +0000

    added mongo to skuv

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, request
 import skuv
-print(skuv)
+
 app = Flask('app')
 
 @app.route('/')
--- api/skuv.py
+++ api/skuv.py
@@ -1,11 +1,26 @@
 from flask import Flask, request
+import pymongo
+from bson.objectid import ObjectId
 import json
+import os
 app = Flask('app')
-print(dict(db))
+if os.path.isfile('mongouri.txt'):
+  connectionstring = open('mongouri.txt').read().strip()
+else:
+  connectionstring = os.environ.get('MONGO_URI')
+cluster = pymongo.MongoClient(connectionstring)
+database = cluster['replit-text-games']
+data = database['sk-uv']
+objid = ObjectId('65b731e556fdaed03a736e72')
+doc = data.find_one({'_id':objid})
+del doc['_id']
+db = doc
 def addkeytoall(key, val):
   for i in db:
     db[i][key] = val
-
+def save():
+  data.update_one({'_id':objid}, {'$set':db})
+  
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
@@ -19,11 +34,13 @@ def savepost():
   userdata = json.loads(request.form['userdata'])
   userid = request.args['userid'][::-1]
   db[userid] = userdata
+  save()
   return 'saved'
 
 @app.route('/getdata', methods=['GET'])
 def getdata():
   enddb = {}
   for key in dict(db):
-    enddb[key] = json.loads(db.get_raw(key))
+    enddb[key] = db[key]
+  save()
   return enddb
